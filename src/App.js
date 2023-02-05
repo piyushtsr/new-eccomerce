ac5d1b2d21e011bf30b7a6d8cb7e0d384115ffe5
piyush77 <piyush@troopr.io>
2023-02-06 01:40:25 +0530

    added scss and some alignments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss'
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
 
-function App() {
   return (
-    <div className="App">
-     
+    <div className='categories-container'>
+      {categories.map(({ title }) => (
+        <div className='category-container'>
+          <div className='background-image' />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
